d1fde71d6c826607859bed5dcd6783b251c9cf38
Jefel John Maitem <jefel@Jefels-MacBook-Air.local>
2026-02-28 09:24:01 +0800

    pathadjusted

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/jefel-john-espanola/", // <--- ADD THIS LINE
+  base: "/jefel-john-espanola/", // Your repository name with hyphens
   optimizeDeps: {
     exclude: ['lucide-react'],
-  },
+  }
 });
