694533a5a13ec6415c1c0691ea4391a162cea64f
Jefel John Maitem <jefel@Jefels-MacBook-Air.local>
2026-02-28 09:17:56 +0800

    padthadjusted

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,11 +1,1 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/jefel-john-espanola/", // <--- ADD THIS LINE
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+/// <reference types="vite/client" />
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/jefel-john-espanola/", // <--- ADD THIS LINE
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
